d35cda0e530ac790ff1b689bb6aa35eb71f993ec
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-03-15 08:36:20 +0200

    Renaming `ViewController` to `HomeViewController`

--- ThriveApp/HomeViewController.swift
+++ ThriveApp/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  ThriveApp
 //
 //  Created by Vadims Vorobjovs on 14/03/2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
