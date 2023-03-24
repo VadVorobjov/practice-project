ebc06e0b7abcffa9c097ff2cb2f3ca9b561eea3a
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-03-24 17:07:48 +0200

    Initiating usage of SwiftUI

---
+++ ThriveApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThriveApp
+//
+//  Created by Vadims Vorobjovs on 24/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack{}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
