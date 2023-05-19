9b507733080d1a3923378cdbc4f96eff70caf593
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-05-19 22:22:41 +0300

    Moving `TaskInitiationNavigationType` to `HomeView`

--- ThriveApp/ThriveApp/UI/SwiftUI/AppTab/AppTabView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/AppTab/AppTabView.swift
@@ -63,31 +63,3 @@ extension Task {
         ]
     }
 }
-
-enum TaskInitiationNavigationType {
-    case name(Navigation)
-    case description(backAction: () -> Void, completion: (String) -> Void)
-    
-    var view: AnyView {
-        switch self {
-        case .name(let navigation):
-            return AnyView(TaskInitiationView(navigation: navigation, task: Task(name: "", description: "")))
-        case .description(let backAction, let completion):
-            return AnyView(TaskDescriptionInitiationCell(backAction: backAction, completion: completion))
-        }
-    }
-}
-
-extension TaskInitiationNavigationType: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        return hasher.combine(identifier)
-    }
-    
-    var identifier: String {
-        return UUID().uuidString
-    }
-    
-    static func == (lhs: TaskInitiationNavigationType, rhs: TaskInitiationNavigationType) -> Bool {
-        return lhs.identifier == rhs.identifier
-    }
-}
