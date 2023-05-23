4161ea8c0b0f2d720e8a812d02b5ab76f88cdad7
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-05-23 09:10:35 +0300

    Adding `Task` model

---
+++ Thrive/Thrive/Task Feature/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  Thrive
+//
+//  Created by Vadims Vorobjovs on 21/05/2023.
+//
+
+struct Task {
+    let id: UUID
+       let name: String
+       let description: String?
+       let date: Date
+}
