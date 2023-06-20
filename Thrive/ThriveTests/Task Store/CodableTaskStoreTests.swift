580fb2c82d071bca7febf8767dc74984c4fe42b9
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-06-20 13:54:12 +0300

    Retrieving from empty store delivers same empty result(no side-effects)

--- Thrive/ThriveTests/Task Store/CodableTaskStoreTests.swift
+++ Thrive/ThriveTests/Task Store/CodableTaskStoreTests.swift
@@ -24,11 +24,33 @@ final class CodableTaskStoreTests: XCTestCase {
             switch result {
             case .empty:
                 break
+                
             default:
                 XCTFail("Expected empty result, got \(result) instead")
             }
             
             exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmtpyStore() {
+        let sut = CodableTaskStore()
+        let exp = expectation(description: "Wait for retreival completion")
+        
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                    
+                default:
+                    XCTFail("Expected retrieving twice from empty store to deliver same result, got \(firstResult) and \(secondResult) instead")
+                }
+                
+                exp.fulfill()
+            }
         }
         
         wait(for: [exp], timeout: 1.0)
