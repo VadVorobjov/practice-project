0384009087e15ed4a03988d5521b67696edebee0
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-07-04 21:43:41 +0300

    Proved that `CodableTaskStore` side-effects run serially.

--- Thrive/ThriveTests/Task Store/CodableTaskStoreTests.swift
+++ Thrive/ThriveTests/Task Store/CodableTaskStoreTests.swift
@@ -151,6 +151,34 @@ final class CodableTaskStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Expected deletion to deliver an error")
     }
     
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        let task = uniqueTask().toLocal()
+        var completedOperationsInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(task) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.delete(task) { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueTask().toLocal()) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially, but operations finished in the wrong order")
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> TaskStore {
