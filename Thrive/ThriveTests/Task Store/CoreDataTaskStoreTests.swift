d4ddf5112b55867fa532a5dfc8b8f4d226e2f621
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-07-09 19:43:47 +0300

    Adding empty CoreData TaskStoreSpecs

---
+++ Thrive/ThriveTests/Task Store/CoreDataTaskStoreTests.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataTaskStoreTests.swift
+//  ThriveTests
+//
+//  Created by Vadims Vorobjovs on 09/07/2023.
+//
+
+import XCTest
+
+final class CoreDataTaskStoreTests: XCTestCase, TaskStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyStore() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmtpyStore() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNoneEmptyStore() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyStore() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyStore() {
+        
+    }
+    
+    func test_insert_applyValueToPrevioslyInsertedValues() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyStore() {
+        
+    }
+    
+    func test_delete_onNonEmptyStoreDeletesProvidedTask() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
