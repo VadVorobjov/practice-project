fe8208a66f859e60939191f6b669a04cda66eca1
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-07-09 20:07:17 +0300

    `CoreDataTaskStore.retrieve()` query delivers empty on empty store.

--- Thrive/ThriveTests/Task Store/CoreDataTaskStoreTests.swift
+++ Thrive/ThriveTests/Task Store/CoreDataTaskStoreTests.swift
@@ -6,11 +6,29 @@
 //
 
 import XCTest
+import Thrive
+
+final class CoreDataTaskStore: TaskStore {
+    
+    func insert(_ item: Thrive.LocalTask, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    func delete(_ item: Thrive.LocalTask, completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        return completion(.empty)
+    }
+}
 
 final class CoreDataTaskStoreTests: XCTestCase, TaskStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyStore() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversEmptyOnEmptyStore(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmtpyStore() {
@@ -43,5 +61,13 @@ final class CoreDataTaskStoreTests: XCTestCase, TaskStoreSpecs {
     
     func test_storeSideEffects_runSerially() {
         
+    }
+    
+    // MARK: - Heleprs
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> TaskStore {
+        let sut = CoreDataTaskStore()
+        trackMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 }
