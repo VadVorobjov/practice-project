4d39be44bc04d2409215defe7e483cd608a4f5f8
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-07-13 23:40:28 +0300

    Adding `ThriveTaskStoreIntegrationTests` target to use for Integration testing (separating integration & unit tests)

---
+++ Thrive/ThriveTaskStoreIntegrationTests/ThriveTaskStoreIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  ThriveTaskStoreIntegrationTests.swift
+//  ThriveTaskStoreIntegrationTests
+//
+//  Created by Vadims Vorobjovs on 13/07/2023.
+//
+
+import XCTest
+
+final class ThriveTaskStoreIntegrationTests: XCTestCase {
+
+}
