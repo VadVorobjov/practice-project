3c87ccf8b57f46459e6b077cd883fed90034b3c8
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-07-20 00:28:11 +0300

    Adding Test plans for `ThriveiOS` and `ThriveiOSTests`

---
+++ Thrive/ThriveiOS/ThriveiOS.h
@@ -0,0 +1,18 @@
+//
+//  ThriveiOS.h
+//  Thrive
+//
+//  Created by Vadims Vorobjovs on 20/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThriveiOS.
+FOUNDATION_EXPORT double ThriveiOSVersionNumber;
+
+//! Project version string for Thrive.
+FOUNDATION_EXPORT const unsigned char ThriveiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thrive/PublicHeader.h>
+
+
--- Thrive/ThriveiOSTests/ThriveiOSTests.swift
+++ Thrive/ThriveiOSTests/ThriveiOSTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import ThriveiOS
+import ThriveiOS
 
 final class ThriveiOSTests: XCTestCase {
 
