a9916b06c39afc77dfc8a8772c5cd1c070bdb8c0
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-01 21:53:32 +0300

    Adding `ExtensibleView`.

---
+++ ThriveApp/ThriveApp/UI/SwiftUI/ExtensibleView.swift
@@ -0,0 +1,20 @@
+//
+//  ExtensibleView.swift
+//  ThriveApp
+//
+//  Created by Vadims Vorobjovs on 01/08/2023.
+//
+
+import SwiftUI
+
+struct ExtensibleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExtensibleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtensibleView()
+    }
+}
