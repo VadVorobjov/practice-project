9686644ea3ceec7e8ff33371efa592d7b64e71b3
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-01 17:13:21 +0300

    Modifying `TaskDescriptionInitiationView` to align with the design.

--- ThriveApp/ThriveApp/UI/SwiftUI/Cell/TaskDescriptionInitiationView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/Cell/TaskDescriptionInitiationView.swift
@@ -19,27 +19,33 @@ struct TaskDescriptionInitiationView: View {
     }
     
     var body: some View {
-        
-        VStack {
-            TextEditorView(text: $description, title: "Description")
-            
-            HStack {
+        ZStack {
+            VStack(alignment: .center, spacing: 0) {
+                TitleTextView(title: "Description")
                 
-                Button {
-                    backAction()
-                } label: {
-                    Image(systemName: "arrow.left")
-                        .foregroundColor(.black)
-                        .bold()
+                TextEditorView(text: $description, title: "Description")
+                    .padding(.top, 20)
+                
+                HStack(alignment: .center) {
+                    Button {
+                        backAction()
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.black)
+                            .font(Font.system(size: 26, weight: .bold))
+                    }
+                    
+                    Button("Next", action: {
+                        completion(description)
+                    })
+                    .buttonStyle(MainButtonStyle())
+                    .padding(.leading, 25)
                 }
+                .padding(.top, 25)
                 
-                Button("Next", action: {
-                    completion(description)
-                })
-                .buttonStyle(MainButtonStyle())
-                .padding(.leading)
             }
-
+            .padding(EdgeInsets(top: 20, leading: 20, bottom: 30, trailing: 20))
+            .modifier(Elevation(color: .Background.Elevation.primary))
         }
         .padding()
     }
