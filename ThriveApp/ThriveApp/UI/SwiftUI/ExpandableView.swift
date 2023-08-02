536c3a824b4cfdee1c23830f0b58acbdf4df7cd7
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-02 21:04:34 +0300

    Modifying `ExpandableView` to align with the design.

--- ThriveApp/ThriveApp/UI/SwiftUI/ExpandableView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/ExpandableView.swift
@@ -10,34 +10,35 @@ import SwiftUI
 struct ExpandableView<Content: View>: View {
     let title: String
     let content: () -> Content
-
+    
     @State private var expanded = false
-        
+    
     var body: some View {
         VStack {
-            Button {
-                withAnimation {
-                    expanded.toggle()
+            HStack(spacing: 0) {
+                Button {
+                    withAnimation { expanded.toggle() }
+                } label: {
+                    Image("chevron.up")
+                        .rotationEffect(.degrees(expanded ? 180 : 0))
+                    Text(title)
+                        .multilineTextAlignment(.leading)
+                        .font(.system(size: 18, weight: .medium))
+                        .foregroundColor(.Label.primary)
                 }
-            } label: {
-                Image("chevron.up")
-                    .rotationEffect(.degrees(expanded ? 180 : 0))
-                Text(title)
-                    .font(.system(size: 18, weight: .medium))
+                .buttonStyle(.plain)
             }
-            .buttonStyle(.plain)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .foregroundColor(.Label.primary)
+            .frame(maxWidth: .infinity, maxHeight: 40, alignment: .leading)
             
             if expanded {
                 HStack {
                     content()
-                        .padding(.top, 5)
+                        .padding(.bottom, 10)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
         }
-        .padding()
+        .padding(.horizontal, 15)
         .background(Color.Background.Elevation.primary)
         .cornerRadius(10)
     }
