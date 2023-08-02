906e0f9f345b95ef28d316bb806d65e0bfa093ac
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-02 20:05:45 +0300

    Implementing `ExtensibleView`.

--- ThriveApp/ThriveApp/UI/SwiftUI/ExtensibleView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/ExtensibleView.swift
@@ -7,14 +7,53 @@
 
 import SwiftUI
 
-struct ExtensibleView: View {
+struct ExtensibleView<Content: View>: View {
+    let title: String
+    let content: () -> Content
+
+    @State private var extended = false
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                withAnimation {
+                    extended.toggle()
+                }
+            } label: {
+                Image("chevron.up")
+                    .rotationEffect(.degrees(extended ? 180 : 0))
+                Text(title)
+                    .font(.system(size: 18, weight: .medium))
+            }
+            .buttonStyle(.plain)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundColor(.Label.primary)
+            
+            if extended {
+                HStack {
+                    content()
+                        .padding(.top, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .padding()
+        .background(Color.Background.Elevation.primary)
+        .cornerRadius(10)
     }
 }
 
 struct ExtensibleView_Previews: PreviewProvider {
     static var previews: some View {
-        ExtensibleView()
+        ExtensibleView(title: "Desciption", content: {
+            Text("Some description")
+        })
+        .previewLayout(.sizeThatFits)
+
+        ExtensibleView(title: "Desciption", content: {
+            Text("Some description")
+        })
+        .previewLayout(.sizeThatFits)
+        .preferredColorScheme(.dark)
     }
 }
