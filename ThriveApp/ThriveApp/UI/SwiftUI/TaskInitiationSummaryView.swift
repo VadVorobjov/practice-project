25e1291387e2f0100c0bc23d4d5c4062160ae048
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-04 18:08:56 +0300

    Adding partially implemented design for `Summary view`(Title, description, category)

--- ThriveApp/ThriveApp/UI/SwiftUI/TaskInitiationSummaryView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/TaskInitiationSummaryView.swift
@@ -10,37 +10,58 @@ import SwiftUI
 struct TaskInitiationSummaryView: View {
     let task: Task
     let initiated: (Task) -> Void
-    
+
+    @State private var expandDescription = false
+
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             customBackgroundView()
-            
-            VStack(alignment: .center) {
-                VStack(alignment: .leading) {
-                    Text("Name").bold()
-                    Text(task.name)
-                        .padding(.bottom)
-                    
-                    Text("Description").bold()
-                    Text(task.description)
-                        .lineLimit(3)
+
+            HStack(alignment: .top) {
+                VStack(alignment: .center, spacing: 0) {
+                    TitleTextView(title: task.name, font: .system(size: 24), fontWeight: .semibold)
+                        .shadow(radius: 1, x: 1, y: 1)
+                        .multilineTextAlignment(.center)
+
+                    if !task.description.isEmpty {
+                        VStack (alignment: .leading) {
+                            Text(task.description)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.system(size: 17))
+                                .fontWeight(.medium)
+                                .padding(.bottom, 5)
+                                .lineLimit(expandDescription ? .max : 3)
+                                .contentTransition(.opacity)
+                            Button {
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    expandDescription.toggle()
+                                }
+                            } label: {
+                                Text(expandDescription ? "Less" : "More")
+                                    .multilineTextAlignment(.leading)
+                                    .font(.title3)
+//                                    .foregroundColor(.black)
+                            }
+
+                        }
+                        .padding(.top)
+                    }
+
+                    ExpandableView(title: "Categories") {
+                        Text(task.description)
+                    }
+                    .padding(.top)
                 }
-                
-                Button("Initiate") {
-                    initiated(task)
-                }
-                .buttonStyle(MainButtonStyle())
-                .padding(.top)
+                .padding()
             }
-            .padding()
         }
     }
 }
 
 struct TaskInitiationSummaryView_Previews: PreviewProvider {
     static private var task = Task(
-        name: "Road to greatnes",
-        description: "Fulfill your path with goals & levels."
+        name: "Walk da Dog",
+        description: "Like its name implies, SwiftUI’s ZStack type is the Z-axis equivalent of the horizontally-oriented HStack and the vertical VStack. When placing multiple views within a ZStack"
     )
 
     static var previews: some View {
