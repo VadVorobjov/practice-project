866cb6974879b57debd1760b7ad1c2754fcce489
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-08-29 22:55:15 +0300

    feat: Adding `Criteria` type to represent model for user selected criterias for their Command

--- Thrive/Thrive/Task Feature/Command.swift
+++ Thrive/Thrive/Task Feature/Command.swift
@@ -9,12 +9,21 @@ public struct Command: Equatable, Identifiable {
     public let id: UUID
     public var name: String
     public let description: String?
+    public let criterias: [Criteria]
     public let date: Date
     
-    public init(id: UUID = UUID(), name: String, description: String? = nil, date: Date) {
+    public init(id: UUID = UUID(), name: String, description: String? = nil, criterias: [Criteria] = [], date: Date) {
         self.id = id
         self.name = name
         self.description = description
+        self.criterias = criterias
         self.date = date
     }
+}
+
+public struct Criteria: Equatable, Identifiable {
+    public var id: UUID
+    
+    public let name: String
+    public let isChecked: Bool
 }
