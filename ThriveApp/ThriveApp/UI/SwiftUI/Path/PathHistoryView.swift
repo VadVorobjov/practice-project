bbb8215e70cf367368a34d5ddda6b2db48805a96
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-09-09 17:38:49 +0300

    feat: Adding history output of created commands.

--- ThriveApp/ThriveApp/UI/SwiftUI/Path/PathHistoryView.swift
+++ ThriveApp/ThriveApp/UI/SwiftUI/Path/PathHistoryView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 import Thrive
 
 struct PathHistoryView: View {
-    @ObservedObject private var model: CommandViewModel
+    @ObservedObject private var model: CommandsViewModel
     
-    init(model: CommandViewModel) {
+    init(model: CommandsViewModel) {
         self.model = model
     }
     
     var body: some View {
-        VStack {
-            List(model.commands) { command in
-                Text(command.name)
+        ZStack {
+            customBackgroundView()
+            
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 0) {
+                    ForEach(model.commands) { command in
+                        PathHistoryItemView(model: CommandViewModel(command: command))
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 5)
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+            .onAppear {
+                model.loadCommands()
             }
         }
     }
@@ -28,15 +40,17 @@ struct PathHistoryView_Preview: PreviewProvider {
     // TODO: unify duplications(CommandCreateView)
     static var previews: some View {
         let loader = LocalCommandLoader(store: NullStore())
-        let model = CommandViewModel(loader: loader)
+        let model = CommandsViewModel(loader: loader)
         model.commands = [
             Command(name: "Walk Da Dog", description: "description", date: .init()),
             Command(name: "Pet Marcus", description: "description", date: .init()),
-            Command(name: "Write to Sandra", description: "description", date: .init())
+            Command(name: "Write to Sandra", description: "description", date: .init()),
+            Command(name: "Pet Marcus", description: "description", date: .init()),
+            Command(name: "Pet Marcus", description: "description", date: .init()),
+            Command(name: "Pet Marcus", description: "description", date: .init())
         ]
         
         return PathHistoryView(model: model)
             .environmentObject(model)
     }
-    
 }
