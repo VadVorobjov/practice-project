43476ee1d985ef6cf357606ad49780fdc2b3cf48
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-09-15 20:23:12 +0300

    feat: Moving `CommandViewModel` to its own file.

---
+++ Thrive/Thrive/Task Presentation/CommandViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CommandViewModel.swift
+//  Thrive
+//
+//  Created by Vadims Vorobjovs on 15/09/2023.
+//
+
+public class CommandViewModel: ObservableObject {
+    private let command: Command
+    
+    public init(command: Command) {
+        self.command = command
+    }
+    
+    public lazy var commandName: String = {
+        command.name
+    }()
+    
+    public lazy var date: String = {
+        formatedDate(date: command.date)
+    }()
+    
+    private func formatedDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter.string(from: date)
+    }
+}
--- Thrive/Thrive/Task Presentation/CommandsViewModel.swift
+++ Thrive/Thrive/Task Presentation/CommandsViewModel.swift
@@ -7,28 +7,6 @@
 
 import Foundation
 
-public class CommandViewModel: ObservableObject {
-    private let command: Command
-    
-    public init(command: Command) {
-        self.command = command
-    }
-    
-    public lazy var commandName: String = {
-        command.name
-    }()
-    
-    public lazy var date: String = {
-        formatedDate(date: command.date)
-    }()
-    
-    private func formatedDate(date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return formatter.string(from: date)
-    }
-}
-
 public class CommandsViewModel: ObservableObject {
     private let loader: CommandLoader
 
