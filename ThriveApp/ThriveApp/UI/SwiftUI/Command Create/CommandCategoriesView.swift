ef95c7121d51bb48fb6f1bd10f41d4ac18c071c3
Vadim Vorobjov <vad.vorobjov@gmail.com>
2023-09-19 21:07:44 +0300

    feat: Adding `CommandCategoriesView`

---
+++ ThriveApp/ThriveApp/UI/SwiftUI/Command Create/CommandCategoriesView.swift
@@ -0,0 +1,20 @@
+//
+//  CommandCategoriesView.swift
+//  ThriveApp
+//
+//  Created by Vadims Vorobjovs on 19/09/2023.
+//
+
+import SwiftUI
+
+struct CommandCategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommandCategoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommandCategoriesView()
+    }
+}
